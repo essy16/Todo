17ef9f598e25b22ed14e9306d46dbc597cb84cbb
Esther Cynthia <58663564+essy16@users.noreply.github.com>
2023-09-09 19:54:55 +0300

    Update AddEditEvent.kt
    Code refactoring

--- app/src/main/java/com/essycynthia/todo/ui/add_edit_todo/AddEditEvent.kt
+++ app/src/main/java/com/essycynthia/todo/ui/add_edit_todo/AddEditEvent.kt
@@ -1,7 +1,7 @@
 package com.essycynthia.todo.ui.add_edit_todo
 
 sealed class AddEditEvent {
-    data class OnTitleChanged(var title: String) : AddEditEvent()
-    data class OnDescriptionChanged(var description: String) : AddEditEvent()
+    data class OnTitleChanged(val title: String) : AddEditEvent()
+    data class OnDescriptionChanged(val description: String) : AddEditEvent()
     object OnSaveClick : AddEditEvent()
 }
